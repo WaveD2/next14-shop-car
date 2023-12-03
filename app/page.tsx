d096b1f389723c4f3cee8cc09b38aa7aa2473a58
WaveD2 <tungdev64@gmail.com>
2023-12-03 10:17:48 +0700

    v1.1.10

---
+++ app/page.tsx
@@ -0,0 +1,59 @@
+import { fetchCars } from "@utils";
+import { HomeProps } from "@types";
+import { fuels, yearsOfProduction } from "@constants";
+import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@components";
+
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Danh mục ô tô</h1>
+          <p>Những chiếc xe bạn có thể thích</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="Loại" options={fuels} />
+            <CustomFilter title="Năm" options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">
+              Có lỗi xảy ra. Vui lòng thử lại
+            </h2>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
---
+++ constants/index.ts
@@ -0,0 +1,93 @@
+export const manufacturers = [
+  "Acura",
+  "Alfa Romeo",
+  "Aston Martin",
+  "Audi",
+  "Bentley",
+  "BMW",
+  "Buick",
+  "Cadillac",
+  "Chevrolet",
+  "Chrysler",
+  "Citroen",
+  "Dodge",
+  "Ferrari",
+  "Fiat",
+  "Ford",
+  "GMC",
+  "Honda",
+  "Hyundai",
+  "Infiniti",
+  "Jaguar",
+  "Jeep",
+  "Kia",
+  "Lamborghini",
+  "Land Rover",
+  "Lexus",
+  "Lincoln",
+  "Maserati",
+  "Mazda",
+  "McLaren",
+  "Mercedes-Benz",
+  "MINI",
+  "Mitsubishi",
+  "Nissan",
+  "Porsche",
+  "Ram",
+  "Rolls-Royce",
+  "Subaru",
+  "Tesla",
+  "Toyota",
+  "Volkswagen",
+  "Volvo",
+];
+
+export const yearsOfProduction = [
+  { title: "Năm", value: "" },
+  { title: "2015", value: "2015" },
+  { title: "2016", value: "2016" },
+  { title: "2017", value: "2017" },
+  { title: "2018", value: "2018" },
+  { title: "2019", value: "2019" },
+  { title: "2020", value: "2020" },
+  { title: "2021", value: "2021" },
+  { title: "2022", value: "2022" },
+  { title: "2023", value: "2023" },
+  { title: "2024", value: "2024" },
+];
+
+export const fuels = [
+  {
+    title: "Tất cả",
+    value: "",
+  },
+  {
+    title: "Xe xăng",
+    value: "Gas",
+  },
+  {
+    title: "Xe dầu",
+    value: "Electricity",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Xe hơi WaveD",
+    links: [
+      { title: "37, Trần Phú, Hà Đông, Hà Nội", url: "/" },
+      { title: "0988233**", url: "/" },
+      { title: "tungdev64@gmail.com", url: "/" },
+      { title: "Mua bán, trao đổi, ký gửi ô tô các loại", url: "/" },
+    ],
+  },
+
+  {
+    title: "Liện hệ",
+    links: [
+      { title: "Instagram", url: "/" },
+      { title: "Facebook", url: "/" },
+      { title: "Gmail", url: "/" },
+    ],
+  },
+];
